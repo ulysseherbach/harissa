b3ad69b5932457fea11b78d45b14fbbbb4b81e8a
Ulysse <35532736+ulysseherbach@users.noreply.github.com>
2019-05-10 17:49:55 +0200

    Update __init__.py

--- harissa/__init__.py
+++ harissa/__init__.py
@@ -6,11 +6,15 @@ Tools for mechanistic-based gene network inference
 
 Current subpackages
 -------------------
-autoactiv : auto-activation inference (network hyperparameters)
-automodel : network inference using a mechanistic-based random field
-grnsim : stochastic simulation of the corresponding dynamical model
+autoactiv: auto-activation inference (network hyperparameters)
+automodel: network inference using a mechanistic-based random field
+grnsim: stochastic simulation of the corresponding dynamical model
+hartree: network inference using a self-consistent proteomic field
 
+Author: Ulysse Herbach (ulysse.herbach@inria.fr)
 """
-__version__ = '0.1.3'
-__author__ = 'Ulysse Herbach'
+__version__ = '0.2'
 __all__ = []
+
+from .hartree import SingleCellData
+__all__ += ['SingleCellData']

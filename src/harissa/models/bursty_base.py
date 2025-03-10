Fix formula

src/harissa/models/bursty_base.py
@@ -77,10 +77,10 @@ class BurstyBase:
         x = x - xt
         # Scaling factors
         e0 = np.exp(d * t)
-        e1 = np.exp(k * d * t)
+        e1 = np.exp(k * t)
         e1[t == 0] = 0 # Discarded
         r1 = b * (e0 - 1)
-        r2 = k * (x >= 0) / (e1 - 1)
+        r2 = (k/d) * (x >= 0) / (e1 - 1)
         # Probability that no burst occurred
         w = np.exp(- k * t)
         # Pre-burst part of the distribution (regularized)
@@ -89,7 +89,7 @@ class BurstyBase:
         # # Option 1: gamma-mixture formula
         # g = r1 * r2 * np.exp(- b * e0 * x) * hyp1f1(k + 1, 2, r1 * x)
         # Option 2: using Kummer's transformation
-        g = r1 * r2 * np.exp(- b * x) * hyp1f1(1 - k, 2, -r1 * x)
+        g = r1 * r2 * np.exp(- b * x) * hyp1f1(1 - k/d, 2, -r1 * x)
         # Overall distribution
         p = w * h + (1 - w) * g
         if t.size == 1:

tests/test_bursty_base.py
@@ -1,5 +1,6 @@
 # BurstyBase model (reduced/hybrid/bursty - single gene - no feedback)
 import numpy as np
+import matplotlib.pyplot as plt
 from harissa.models import BurstyBase
 
 # Path of result files
@@ -7,31 +8,41 @@ result_path = "../examples/results/bursty_base_"
 
 model = BurstyBase()
 model.burst_size = 0.5
-model.burst_frequency = 2
-model.degradation_rate = 1
+model.burst_frequency = 2.5
+model.degradation_rate = 0.8
 
-# Trajectories
+# Compute a single trajectory
 time = np.linspace(0, 10, 1000)
 sim = model.simulate(time, init_state=1)
 
-# Distribution
-x = np.linspace(0, 3, 1000)
-init_state = 1
-time = np.linspace(0, 5, 6)
-p = model.distribution(x, time, init_state)
-
-import matplotlib.pyplot as plt
+# Show the trajectory
 fig = plt.figure(figsize=(12,3))
 plt.plot(sim.t, sim.x)
 plt.xlim(sim.t[0], sim.t[-1])
 plt.ylim(0)
 fig.savefig(result_path + "traj.pdf", bbox_inches='tight')
 
-import matplotlib.pyplot as plt
+# Time-dependent distribution
+x = np.linspace(0, 3, 1000)
+init_state = 1
+time = np.linspace(0, 5, 6)
+p = model.distribution(x, time, init_state)
+
+# Confirm analytical formula
+t = time[1]
+n_cells = 10000
+cell_pop = np.zeros(n_cells)
+for k in range(n_cells):
+    # print(f"Cell {k+1}...")
+    sim = model.simulate(t, init_state)
+    cell_pop[k] = sim.x[0]
+
+# Show time-dependent distribution
 fig = plt.figure(figsize=(8,3))
 for k, t in enumerate(time):
     plt.plot(x, p[k], label=f"t = {t}")
     print(f"t = {t} -> non-zeros: {np.count_nonzero(p[k])}")
+plt.hist(cell_pop, density=True, bins=100, color='lightgray', zorder=0)
 plt.xlim(x[0], x[-1])
 plt.ylim(0, 1)
 plt.legend(loc='upper right')
cb76479a75115796c38b1033c65467bb670d8379
Ulysse Herbach <35532736+ulysseherbach@users.noreply.github.com>
2025-03-10 02:36:37 +0100

    Update bursty model

--- src/harissa/models/_checks.py
+++ src/harissa/models/_checks.py
@@ -3,32 +3,39 @@ Various checks for function parameters within the API.
 """
 import numpy as np
 
-def check_time_points(time):
+def _check_time_points(time):
     """
     Check time points for trajectory simulations.
     """
-    if np.shape(time) == ():
+    if np.ndim(time) == 0:
         time = np.array([time], dtype=float)
-    else:
+    elif np.ndim(time) == 1:
         time = np.array(time, dtype=float)
+    else:
+        raise ValueError("Time points should either be scalar or 1D array.")
     if np.any(time != np.sort(time)):
         raise ValueError("Time points must be given in increasing order.")
     if time[0] < 0:
         raise ValueError("Time points must be nonnegative.")
     return time
 
-def check_init_state(state):
+def _check_init_state(init_state, shape=None):
     """
     Check initial state for trajectory simulations.
     """
-    if np.any(np.array(state) < 0):
+    if isinstance(shape, tuple) and (np.shape(init_state) != shape):
+        if shape == ():
+            raise ValueError("Initial state must be a scalar value.")
+        else:
+            raise ValueError(f"Initial state must have shape {shape}.")
+    if np.any(np.array(init_state) < 0):
         raise ValueError("Initial state must be nonnegative.")
-    return state
+    return init_state
 
 
 # Tests
 if __name__ == '__main__':
     time = np.linspace(0, 10, 100)
-    check_time_points(time)
+    _check_time_points(time)
     state = 0, 1
-    check_init_state(state)
+    _check_init_state(state)
--- src/harissa/models/bursty_base.py
+++ src/harissa/models/bursty_base.py
@@ -2,7 +2,8 @@
 Reduced/Hybrid/Bursty model for a single gene with no feedback
 """
 import numpy as np
-from harissa.models._checks import check_time_points, check_init_state
+from scipy.special import hyp1f1
+from harissa.models._checks import _check_time_points, _check_init_state
 
 class Simulation:
     """
@@ -34,8 +35,8 @@ class BurstyBase:
         burst_frequency = self.burst_frequency
         degradation_rate = self.degradation_rate
         # Check parameters
-        time = check_time_points(time)
-        check_init_state(init_state)
+        time = _check_time_points(time)
+        init_state = _check_init_state(init_state)
         # Number of bursts
         n = np.random.poisson(burst_frequency * time[-1])
         # Burst times and heights
@@ -54,30 +55,59 @@ class BurstyBase:
             k = np.argwhere(t <= u)[-1][0]
             dt = u - t[k]
             traj[i] = x[k] * np.exp(- degradation_rate * dt)
+        if verb:
+            print(f'Simulation generated {n} jumps.')
         return Simulation(time, traj)
 
-
-
-    def conditional_distribution(self, x, time, init_state, smooth=0.0):
+    def distribution(self, x, time, init_state=0.0, smooth=1e-2):
         """
+        Time-dependent distribution conditionally to initial_state.
         """
-        pass
-
-    def propagator(self, x, time, init_state, smooth=0.0):
-        """
-        Alias for `conditional_distribution`.
-        """
-        return self.conditional_distribution(x, time, init_state, smooth)
-
-
-
-
+        b = 1 / self.burst_size
+        k = self.burst_frequency
+        d = self.degradation_rate
+        # Check parameters
+        t = _check_time_points(time).reshape((-1, 1))
+        x0 = _check_init_state(init_state, shape=())
+        # Smoothing standard deviation
+        s = smooth
+        # State when no burst occurred
+        xt = np.exp(- d * t) * x0
+        # Array of translated states
+        x = x - xt
+        # Scaling factors
+        e0 = np.exp(d * t)
+        e1 = np.exp(k * d * t)
+        e1[t == 0] = 0 # Discarded
+        r1 = b * (e0 - 1)
+        r2 = k * (x >= 0) / (e1 - 1)
+        # Probability that no burst occurred
+        w = np.exp(- k * t)
+        # Pre-burst part of the distribution (regularized)
+        h = np.exp(- x**2 / (2 * s**2)) / (np.sqrt(2 * np.pi) * s)
+        # Post-burst part of the distribution
+        # # Option 1: gamma-mixture formula
+        # g = r1 * r2 * np.exp(- b * e0 * x) * hyp1f1(k + 1, 2, r1 * x)
+        # Option 2: using Kummer's transformation
+        g = r1 * r2 * np.exp(- b * x) * hyp1f1(1 - k, 2, -r1 * x)
+        # Overall distribution
+        p = w * h + (1 - w) * g
+        if t.size == 1:
+            p = p[0]
+        return p
 
 
 # Tests
 if __name__ == '__main__':
     model = BurstyBase()
-    time = np.linspace(0, 5, 6)
-    sim = model.simulate(time)
+    # Simulation
+    time = np.linspace(0, 50, 6)
+    sim = model.simulate(time, verb=True)
     print(sim.t)
     print(sim.x)
+    # Distribution
+    x = np.linspace(0, 2, 4)
+    time = 0, 1, 2
+    init_state = 0.5
+    p = model.distribution(x, time, init_state)
+    print(p)
